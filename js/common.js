62edcbe22dc9964bbb526b7dc4799971302ddbfe
uthomeless-er <uthomeless@gmail.com>
2026-06-06 21:28:26 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -57,7 +57,7 @@ const NAV_ITEMS = [
     { href: 'odds.html',        label: 'オッズ' },
     { href: 'submit.html',      label: '買い目' },
     { href: 'predictions.html', label: '予想家の見解' },
-    { href: 'result.html',      label: '結果' },
+    { href: 'result.html',      label: '途中経過' },
     { href: 'help.html',        label: '使い方' },
     { href: 'faq.html',         label: 'Q&A' },
 ];
